06e7a692a9f99668441b5ef41e8fdb19c4a45367
Matthew Wong <fromosia@gmail.com>
2019-09-10 19:05:13 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# EROS
+
+This algorithm was implemented with regards to the paper written by Stephen Smith [1].
+
+## Usage
+
+## Prequisite
+
+## Reference
+1. Smith, Stephen, and Mark Jenkinson. "Accurate robust symmetry estimation." International Conference on Medical Image Computing and Computer-Assisted Intervention. Springer, Berlin, Heidelberg, 1999.
---
+++ eros/eros.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2
+
+def compute_mask(im_3D):
+    assert isinstance(im_3D, np.ndarray), "Non-numpy input encountered."
+    counts, bin = np.histogram(im_3D, bins=100)
+    im_min, im_max = bin[5], bin[-5]
+
+    threshold = im_min + 0.05*(im_max - im_min)
+    return im_3D > threshold
+
+def resample_at_angle(im_2D, phi, cent_of_rotation=None):
+    row, col = im_2D.shape
+
+    if cent_of_rotation is None:
+        moments = cv2.moments(im_2D)
+        cx = int(moments['m10']/moments['m00'])
+        cy = int(moments['m01']/moments['m00'])
+    else:
+        cx, cy = cent_of_rotation
+
+    A = cv2.getRotationMatrix2D((cx, cy), phi, 1)
+    warpped_im_2D = cv2.warpAffine(im_2D, A, (row, col))
+    return warpped_im_2D, cx, cy
+
+
+
+def eros(im_3D, angular_res):
+    out = []
+    for z in range(len(im_3D)):
+        # z = 24
+        cx, cy = None, None
+        row, col = im_3D[z].shape
+        mid = col // 2
+        angles = np.arange(180 // angular_res)*angular_res
+        peak_scores = []
+        for phi in angles:
+            if cx is None:
+                rot, cx, cy = resample_at_angle(im_3D[z], phi)
+            else:
+                rot, cx, cy = resample_at_angle(im_3D[z], phi, [cx, cy])
+
+            sym_score_map = np.zeros_like(rot)
+            for w in range(1, col-1):
+                if w < mid:
+                    N = (0, 2 * w)
+                else:
+                    N = (2*w - col, col)
+
+                sub_sampled_rot = rot[:,N[0]:N[1]]
+                sub_sampled_flipped_rot = np.fliplr(sub_sampled_rot)
+                sym_score_map[:,w] = (np.sum(np.abs(sub_sampled_rot + sub_sampled_flipped_rot), axis=1) - np.sum(np.abs(sub_sampled_rot - sub_sampled_flipped_rot), axis=1)) / \
+                            (np.sum(np.abs(sub_sampled_rot + sub_sampled_flipped_rot), axis=1) + np.sum(np.abs(sub_sampled_rot - sub_sampled_flipped_rot), axis=1))
+
+            sym_score_map[np.isnan(sym_score_map)] = 0
+            peak_score_per_row = np.max(sym_score_map, axis=1)
+            peak_scores.append(np.sum(peak_score_per_row))
+
+        best_angle = angles[np.argmax(peak_scores)]
+        out.append([best_angle, (cx,cy)])
+    return out
+
